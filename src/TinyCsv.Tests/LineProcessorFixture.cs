e9c9ed1e2df221c1a2f3998a8c7db6ef9898ea82
Steven Robbins <ste.robbins@gmail.com>
2017-02-16 19:00:59 +0000

    Initial commit with very basic csv processing working

---
+++ src/TinyCsv.Tests/LineProcessorFixture.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using Should;
+using Xunit;
+
+namespace TinyCsv.Tests
+{
+    public class LineProcessorFixture
+    {
+        private LineProcessor _processor;
+
+        public LineProcessorFixture()
+        {
+            this._processor = LineProcessor.Csv;
+        }
+
+        [Fact]
+        public void Should_handle_basic_line()
+        {
+            // Given
+            var line = "this,that,the other";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new [] { "this", "that", "the other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_handle_empty_elements()
+        {
+            // Given
+            var line = "this,,the other";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "", "the other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_handle_quotes()
+        {
+            // Given
+            var line = "\"this\",\"that\",'the other'";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "that", "the other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_handle_commas_in_quotes()
+        {
+            // Given
+            var line = "\"this\",\"that\",'the,other'";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "that", "the,other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_handle_different_quote_character_in_quotes()
+        {
+            // Given
+            var line = "\"this\",\"that\",'the\"other'";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "that", "the\"other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_handle_escaped_quotes()
+        {
+            // Given
+            var line = "\"this\",\"that\",'the\'\'other'";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "that", "the'other" }).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Should_only_assume_quotes_if_initial_character_is_a_quote()
+        {
+            // Given
+            var line = "this,that,\"the other\"";
+
+            // When
+            var result = this._processor.Process(line);
+
+            // Then
+            result.SequenceEqual(new[] { "this", "that", "\"the other\"" }).ShouldBeTrue();
+        }
+    }
+}
---
+++ src/TinyCsv/LineProcessor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyCsv
+{
+    public class LineProcessor
+    {
+        private readonly char _separator;
+
+        public LineProcessor(char separator)
+        {
+            _separator = separator;
+        }
+
+        public static LineProcessor Csv { get {  return new LineProcessor(','); } }
+
+        public IEnumerable<string> Process(string line)
+        {
+            var lineCharacters = line.ToCharArray();
+            var currentCharacterIndex = 0;
+            var currentValueBuilder = new StringBuilder();
+            char? currentQuoteCharacter = null;
+
+            while (currentCharacterIndex < lineCharacters.Length)
+            {
+                var currentCharacter = lineCharacters[currentCharacterIndex];
+
+                if (this.IsSeparator(currentCharacter))
+                {
+                    yield return currentValueBuilder.ToString();
+                    
+                    currentCharacterIndex++;
+
+                    currentValueBuilder.Clear();
+
+                    continue;
+                }
+
+                currentValueBuilder.Append(currentCharacter);
+                currentCharacterIndex++;
+            }
+
+            yield return currentValueBuilder.ToString();
+        }
+
+        private bool IsSeparator(char currentCharacter)
+        {
+            return currentCharacter == _separator;
+        }
+    }
+}
