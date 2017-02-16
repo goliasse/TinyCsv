19b24975bc7752da982e8bf224b67243c848dae3
Steven Robbins <ste.robbins@gmail.com>
2017-02-16 21:21:52 +0000

    Added license statement and static 'builder' class

--- src/TinyCsv/TinyCsv.cs
+++ src/TinyCsv/TinyCsv.cs
@@ -1,4 +1,30 @@
-﻿using System.Collections.Generic;
+﻿//===============================================================================
+// TinyCsv
+//
+// https://github.com/grumpydev/TinyCsv
+//===============================================================================
+// Copyright © 2017 Steven Robbins  All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//===============================================================================
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -158,6 +184,32 @@ namespace TinyCsv
         private bool IsSeparator(char currentCharacter)
         {
             return currentCharacter == _separator;
+        }
+    }
+
+    public static class Process
+    {
+        public static IEnumerable<IEnumerable<string>> FromCommaSeparatedString(string input, char[] quoteCharacters = null)
+        {
+            var reader = new StringLineReader(input);
+            var processor = new LineProcessor(',', quoteCharacters);
+
+            return Execute(reader, processor);
+        }        
+        
+        public static IEnumerable<IEnumerable<string>> FromTabSeparatedString(string input, char[] quoteCharacters = null)
+        {
+            var reader = new StringLineReader(input);
+            var processor = new LineProcessor('\t', quoteCharacters);
+
+            return Execute(reader, processor);
+        }
+
+        private static IEnumerable<IEnumerable<string>> Execute(ILineReader reader, ILineProcessor processor)
+        {
+            var fileProcessor = new FileProcessor(reader, processor);
+
+            return fileProcessor.Process();
         }
     }
 }
